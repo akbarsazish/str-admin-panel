7cbe9b34c6dbae406e42a6b86afc1ce555bda531
sazishaliakbar@gmail.com <sazishaliakbar@gmail.com>
2023-11-20 11:32:35 +0330

    edit factor part of admin panel

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -232,7 +232,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
 
-    
     <script src="{{url('/resources/assets/js/tableScript.js') }}"></script>
     
     <script>
